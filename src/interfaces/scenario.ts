24babc902dc1cf3478aea3bb326aac463aa50a6c
arnold <arnold@jasny.net>
2023-06-03 21:27:50 -0400

    Allow defining end states. Check reference to states and actions in validate.

--- src/interfaces/scenario.ts
+++ src/interfaces/scenario.ts
@@ -1,74 +1,79 @@
 export type Fn = FnRef | FnEval | FnTpl;
 
 export interface JsonObjectSchema {
-  type?: 'object';
-  title?: string;
-  description?: string;
-  properties?: Record<string, any>;
-  required?: string[];
-  additionalProperties?: boolean | Record<string, any>;
+    type?: 'object';
+    title?: string;
+    description?: string;
+    properties?: Record<string, any>;
+    required?: string[];
+    additionalProperties?: boolean | Record<string, any>;
 }
 
 interface FnRef {
-  '<ref>': string;
+    '<ref>': string;
 }
 
 interface FnEval {
-  '<eval>': string;
+    '<eval>': string;
 }
 
 interface FnTpl {
-  '<tpl>': string;
+    '<tpl>': string;
 }
 
 export interface Action {
-  $schema?: string;
-  title?: string;
-  description?: string | Fn;
-  actor?: string | string[] | Fn;
-  update?: UpdateInstruction | UpdateInstruction[];
-  [_: string]: any
+    $schema?: string;
+    title?: string;
+    description?: string | Fn;
+    actor?: string | string[] | Fn;
+    update?: UpdateInstruction | UpdateInstruction[];
+
+    [_: string]: any
 }
 
 export interface UpdateInstruction {
-  select: string;
-  data?: any | Fn;
-  patch?: boolean;
-  if?: boolean | Fn;
+    select: string;
+    data?: any | Fn;
+    patch?: boolean;
+    if?: boolean | Fn;
 }
 
 export interface SimpleState {
-  title?: string | Fn;
-  instructions?: string | Record<string, string>;
-  on: string;
-  goto: string;
+    title?: string | Fn;
+    instructions?: string | Record<string, string>;
+    on: string;
+    goto: string;
 }
 
 export interface State {
-  title?: string | Fn;
-  instructions?: string | Record<string, string>;
-  transitions: Array<Transition | TimeoutTransition>;
+    title?: string | Fn;
+    instructions?: string | Record<string, string>;
+    transitions: Array<Transition | TimeoutTransition>;
 }
 
 export interface Transition {
-  on: string;
-  if?: boolean | Fn;
-  goto: string;
+    on: string;
+    if?: boolean | Fn;
+    goto: string;
 }
 
 export interface TimeoutTransition {
-  after: string | number;
-  if?: boolean | Fn;
-  goto: string;
+    after: string | number;
+    if?: boolean | Fn;
+    goto: string;
+}
+
+export interface EndState {
+    title?: string | Fn;
 }
 
 export interface Scenario {
-  $schema?: string;
-  title: string;
-  description?: string;
+    $schema?: string;
+    title: string;
+    description?: string;
 
-  actors?: Record<string, JsonObjectSchema>;
-  actions: Record<string, Action>;
-  states: Record<string, State | SimpleState>;
-  assets?: Record<string, JsonObjectSchema>;
+    actors?: Record<string, JsonObjectSchema>;
+    actions: Record<string, Action>;
+    states: Record<string, State | SimpleState | EndState>;
+    assets?: Record<string, JsonObjectSchema>;
 }
--- src/validate.ts
+++ src/validate.ts
@@ -3,6 +3,7 @@ import scenarioSchema from './schemas/v1.0.0/scenario.json';
 import actionSchema from './schemas/v1.0.0/action.json';
 import fnSchema from './schemas/v1.0.0/fn.json';
 import {ErrorObject} from "ajv/lib/types";
+import {Scenario, State} from "./interfaces/scenario";
 
 export interface ValidateFunction {
     (data: any): boolean
@@ -20,6 +21,7 @@ export const validate: ValidateFunction = (scenario: any): boolean => {
     const errors = [
         ...(validateSchema.errors || []),
         ...validateActions(scenario),
+        ...validateStates(scenario),
     ];
 
     validate.errors = errors.length > 0 ? errors : null;
@@ -27,18 +29,75 @@ export const validate: ValidateFunction = (scenario: any): boolean => {
     return errors.length === 0;
 }
 
-function validateActions(scenario): ErrorObject[] {
+function validateActions(scenario: Scenario): ErrorObject[] {
     const errors: ErrorObject[] = [];
 
     if (Object.keys(scenario.actions || {}).length === 0) {
-        errors.push({
-            instancePath: '/actions',
-            message: "must have at least one action",
-            keyword: '',
-            params: {},
-            schemaPath: '',
-        });
+        errors.push(error('/actions', 'must have at least one action'));
     }
 
     return errors;
+}
+
+function validateStates(scenario: Scenario): ErrorObject[] {
+    const actions = Object.keys(scenario.actions || {});
+    const states = Object.keys(scenario.states || {});
+
+    return Object.entries(scenario.states || {}).map(([key, state]) => {
+        const errors: ErrorObject[] = [];
+        if ('on' in state && !actions.includes(state.on)) {
+            errors.push(error(
+                `/states/${key}/on`,
+                'must reference an action',
+                { allowedValues: actions },
+            ));
+        }
+
+        if ('goto' in state && !states.includes(state.goto) && !state.goto.match(/^\(.*\)$/)) {
+            errors.push(error(
+                `/states/${key}/goto`,
+                'must reference a state',
+                { allowedValues: states },
+            ));
+        }
+
+        return [
+            ...errors,
+            ...('transitions' in state ? validateTransitions(state, actions, states) : []),
+        ];
+    }).flat();
+}
+
+function validateTransitions(state: State, actions: string[], states: string[]): ErrorObject[] {
+    const errors: ErrorObject[] = [];
+
+    Object.entries(state.transitions || {}).forEach(([key, transition]) => {
+        if ('on' in transition && !actions.includes(transition.on)) {
+            errors.push(error(
+                `/states/${key}/transitions/${transition}/on`,
+                'must reference an action',
+                { allowedValues: actions },
+            ));
+        }
+
+        if ('goto' in transition && !states.includes(transition.goto) && !transition.goto.match(/^\(.*\)$/)) {
+            errors.push(error(
+                `/states/${key}/transitions/${transition}/goto`,
+                'must reference a state',
+                { allowedValues: states },
+            ));
+        }
+    });
+
+    return errors;
+}
+
+function error(instancePath, message, params: Record<string, any> = {}): ErrorObject {
+    return {
+        instancePath,
+        keyword: '',
+        message,
+        params,
+        schemaPath: '',
+    };
 }
