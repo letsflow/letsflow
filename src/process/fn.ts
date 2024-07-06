236b0a874ec2a05ca5fad591a9fee7c3c1dbfaca
arnold <arnold@jasny.net>
2024-07-06 21:17:17 +0200

    Added custom functions * if * json_serialize * json_parse * sha256 * uuidv5 * re_test * re_match * re_match_all * re_replace

--- src/process/fn.ts
+++ src/process/fn.ts
@@ -1,13 +1,54 @@
-import jmespath, { TYPE_ANY, TYPE_BOOLEAN } from '@letsflow/jmespath';
+import jmespath, { TYPE_ANY, TYPE_BOOLEAN, TYPE_STRING } from '@letsflow/jmespath';
 import { Fn } from '../scenario';
+import { sha256 } from '@noble/hashes/sha256';
+import { bytesToHex } from '@noble/hashes/utils';
+import stringify from 'fast-json-stable-stringify';
+import { v5 as uuidv5, NIL } from 'uuid';
 
 type JSONData = Parameters<typeof jmespath.search>[0];
 
+jmespath.registerFunction('if', ([cond, a, b]: [boolean, any, any?]) => (cond ? a : b), [
+  { types: [TYPE_BOOLEAN] },
+  { types: [TYPE_ANY] },
+  { types: [TYPE_ANY], optional: true },
+]);
+
+jmespath.registerFunction('json_serialize', ([val]: [any]) => stringify(val), [{ types: [TYPE_ANY] }]);
+jmespath.registerFunction('json_parse', ([val]: [string]) => JSON.parse(val), [{ types: [TYPE_STRING] }]);
+
+jmespath.registerFunction('sha256', ([val]: [string]) => bytesToHex(sha256(val)), [{ types: [TYPE_STRING] }]);
+jmespath.registerFunction('uuidv5', ([val, ns]: [string, string | undefined]) => uuidv5(val, ns ?? NIL), [
+  { types: [TYPE_STRING] },
+  { types: [TYPE_STRING], optional: true },
+]);
+
+jmespath.registerFunction('re_test', ([regexp, val]: [string, string]) => parseRegexString(regexp).test(val), [
+  { types: [TYPE_STRING] },
+  { types: [TYPE_STRING] },
+]);
+jmespath.registerFunction('re_match', ([regexp, val]: [string, string]) => val.match(parseRegexString(regexp)), [
+  { types: [TYPE_STRING] },
+  { types: [TYPE_STRING] },
+]);
 jmespath.registerFunction(
-  'if',
-  ([cond, a, b]) => cond ? a : b,
-  [{ types: [TYPE_BOOLEAN] }, { types: [TYPE_ANY] }, { types: [TYPE_ANY], optional: true }],
+  're_match_all',
+  ([regexp, val]: [string, string]) => Array.from(val.matchAll(parseRegexString(regexp))),
+  [{ types: [TYPE_STRING] }, { types: [TYPE_STRING] }],
 );
+jmespath.registerFunction(
+  're_replace',
+  ([regexp, val, repl]: [string, string, string]) => val.replace(parseRegexString(regexp), repl),
+  [{ types: [TYPE_STRING] }, { types: [TYPE_STRING] }, { types: [TYPE_STRING] }],
+);
+
+function parseRegexString(regexString: string): RegExp {
+  // Match the pattern between slashes and any flags after the last slash
+  const match = regexString.match(/^\/(.*?)\/([gimsuy]*)$/);
+  if (!match) {
+    throw new Error('Invalid regex string format');
+  }
+  return new RegExp(match[1], match[2]);
+}
 
 export function applyFn(subject: any, data: Record<string, any>): any {
   if (typeof subject !== 'object' || subject === null || subject instanceof Date) {
@@ -30,8 +71,12 @@ export function applyFn(subject: any, data: Record<string, any>): any {
 }
 
 export function isFn(subject: any): subject is Fn {
-  return typeof subject === 'object' && subject !== null && !(subject instanceof Date)
-    && ('<ref>' in subject || '<sub>' in subject);
+  return (
+    typeof subject === 'object' &&
+    subject !== null &&
+    !(subject instanceof Date) &&
+    ('<ref>' in subject || '<sub>' in subject)
+  );
 }
 
 export function ref(expression: string, data: JSONData): any {
