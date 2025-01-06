975c430af356dc1063246f7a18e2820e75fed36b
arnold <arnold@jasny.net>
2025-01-06 21:46:38 +0100

    Throw error for `etag` if process has no events.

--- src/process/etag.ts
+++ src/process/etag.ts
@@ -1,9 +1,11 @@
 import { Process } from './interfaces/process';
 
 export function etag(process: Process) {
+  if ((process.events ?? []).length === 0) throw new Error('Process has no events');
   return process.events[process.events.length - 1].hash;
 }
 
 export function lastModified(process: Process) {
+  if ((process.events ?? []).length === 0) throw new Error('Process has no events');
   return process.events[process.events.length - 1].timestamp.toUTCString();
 }
