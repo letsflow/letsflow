2533684909d48023bf02944a2c335b75bf1847f6
arnold <arnold@jasny.net>
2025-02-17 15:56:21 +0100

    fix: export `replay` and `migrate`

--- src/process/index.ts
+++ src/process/index.ts
@@ -2,6 +2,7 @@ export * from './interfaces/process';
 export { instantiate } from './instantiate';
 export { step, timeout } from './step';
 export { predict } from './predict';
+export { replay, migrate } from './replay';
 export { etag, lastModified } from './etag';
 export { hasEnded, chain } from './utils';
 export { hash, withHash } from './hash';
